0a9026a8639c0d623f9e54447637459b2f204cd6
Kotaro Kuroda <jolly96k@gmail.com>
2024-04-30 00:29:09 +0900

    feat: ダッシュボード画面を追加

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>Hello, this is your dashboard</div>
+  <div class="container">
+    <div>
+      <div
+        class="alert alert-success alert-dismissible fade show border border-success border-2"
+        role="alert"
+      >
+        <div>こんにちは、{{ userName }}さん。</div>
+        <div>Voygerへようこそ。こちらはダッシュボード画面です。</div>
+        <div>あなたの好きなアーティストを可視化してみましょう。</div>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
   useHead({
     title: "dashboard"
   })
+
+  const userStore = useUserStore()
+  const { userName, isLoggedin, isDemoUser } = storeToRefs(userStore)
 </script>
