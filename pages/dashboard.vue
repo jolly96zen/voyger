16ca7b9d7f4d999ce8e77be8f4123d7ee446fc97
Kotaro Kuroda <jolly96k@gmail.com>
2024-05-13 04:47:26 +0900

    refactor: ファイル名を変更
    アプリの動作に変更はない

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -10,7 +10,7 @@
       <DashboardAlert class="mx-2 my-2 max-w-fit" />
     </div>
     <div class="grid grid-cols-1 justify-items-stretch gap-2 lg:grid-cols-2">
-      <SpotifyRanking class="mx-2 my-2" />
+      <SpotifyArtistRanking class="mx-2 my-2" />
     </div>
   </div>
 </template>
