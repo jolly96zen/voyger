3b6e70e9977ec117728b89c383acc2eba29045a3
Kotaro Kuroda <jolly96k@gmail.com>
2024-05-12 22:30:04 +0900

    add: Spotifyアカウントのセッション延長に対応(試験機能)
    Spotify APIの実行に必要なトークンの有効期限が切れてしまうため

---
+++ utils/spotify-utils.ts
@@ -0,0 +1,14 @@
+export const getSpotifyRefreshToken = async () => {
+  const appConfig = useAppConfig()
+  const supabaseSession = useSupabaseSession()
+
+  return await useLazyFetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { contentType: "application/x-www-form-urlencoded" },
+    body: {
+      grant_type: "refresh_token",
+      refresh_token: supabaseSession.value?.provider_refresh_token ?? "",
+      client_id: appConfig.spotifyClientID
+    }
+  })
+}
