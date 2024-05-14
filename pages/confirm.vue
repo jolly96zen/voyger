049f32b6a405bbc8770fad40c69dc4ab339f2899
Kotaro Kuroda <jolly96k@gmail.com>
2024-05-14 20:28:33 +0900

    refactor: リダイレクト処理を削除
    ルーティングのルールでリダイレクトされるため

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -5,24 +5,4 @@
 <script setup lang="ts">
   definePageMeta({ layout: "center" })
   useHead({ title: "confirm" })
-
-  const supabaseSession = useSupabaseSession()
-
-  watch(
-    supabaseSession,
-    async (): Promise<void> => {
-      if (
-        supabaseSession.value !== null &&
-        supabaseSession.value.provider_refresh_token !== null &&
-        supabaseSession.value.provider_refresh_token !== undefined
-      ) {
-        await new Promise((): void => {
-          setTimeout(() => {
-            return navigateTo("/dashboard")
-          }, 1000)
-        })
-      }
-    },
-    { immediate: true }
-  )
 </script>
